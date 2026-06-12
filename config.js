afef3a1c21e761e8bc702693b277746df61dee56
TaufiqTri08 <144109209+TaufiqTri08@users.noreply.github.com>
2026-06-13 00:00:52 +0700

    chore: Restore config.js to GitHub so Cloudflare Pages deployment works properly

---
+++ config.js
@@ -0,0 +1,16 @@
+export const CONFIG = {
+    // GROQ API endpoint configuration hide
+    API_URL: "https://api.groq.com/openai/v1/chat/completions",
+    API_KEY: "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",
+    MODEL_NAME: "llama-3.1-8b-instant",
+
+    // Dataset Path
+    DATA_PATH: "Sales_BY_Category.csv",
+
+    // UI Constants
+    UI: {
+        NEGATIVE_COLOR: "var(--accent-red)",
+        POSITIVE_COLOR: "var(--accent-green)",
+        NEUTRAL_COLOR: "var(--accent-blue)"
+    }
+};
